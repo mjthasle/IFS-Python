c2a304d0855129cc3d456375b116d80387303090
mjthasle <mjthasle@gmail.com>
2022-11-14 20:47:17 -0500

    Update codespace_function.py

--- IFS Tool/Library/IFS/codespace_function.py
+++ IFS Tool/Library/IFS/codespace_function.py
@@ -1,5 +1,5 @@
 #m is the number of functions in the IFS, and n is the number of desired iterations.
-#The function codes generates a list of all possible lists of integers from 0 to m - 1.
+#The function codes generates a list of all possible lists of length n consisting of integers from 0 to m - 1.
 
 def codes(m, n):
     l = 0
