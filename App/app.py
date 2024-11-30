f68fcdc0044161be6c106f52c2a949a47aeef16e
emilykorfanty <ekorfant@ualberta.ca>
2024-11-30 12:08:45 -0700

    Reset n upon selectbox change

--- App/app.py
+++ App/app.py
@@ -3,16 +3,22 @@ from ifs import *
 
 st.title("Let's draw IFS attractors!")
 st.header("Built-in IFS")
-col1, col2 = st.columns(2, gap="large")
+col1, col2 = st.columns(2, gap = "large")
 
-# input options
-#plt.axis("scaled")
+# built-in IFS options
+box_options = ["Sierpinski Gasket", "Fudgeflake", "Twindragon"]
+
+def reset_n():
+	st.session_state.n = 0
+
+# built-in IFS settings in the left column
 with col1:
-	box_options = ["Sierpinski Gasket", "Fudgeflake", "Twindragon"]
-	option_selected = st.selectbox("Select an IFS attractor to plot", box_options)
-	n = st.number_input("Number of iterations: ", value=0, min_value=0, max_value=10, step=1)
+	option_selected = st.selectbox("Select an IFS attractor to plot", 
+		box_options, on_change = reset_n)
+	n = st.number_input("Number of iterations: ", min_value = 0, max_value = 10, 
+		step = 1, key = "n")
 
-# plot successive iterations in a grid
+# plot the attractor in the right column
 with col2:
 	if option_selected == "Sierpinski Gasket":
 		st.pyplot(gasket().plot(n = n, showaxis = False))  
