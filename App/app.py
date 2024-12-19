99b49355a41a0ea5232d32381ef199005c2b3570
emilykorfanty <ekorfant@ualberta.ca>
2024-12-19 11:05:53 -0700

    Update multiplot

--- App/app.py
+++ App/app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 from ifs import *
+st.set_page_config(layout="wide")
 plt.rcParams.update({'font.size': 35})
-plt.rcParams['figure.figsize'] = (16, 16)
 
 st.title("Let's draw IFS attractors!")
 st.header("Built-in IFS")
+
 multiplot = st.toggle("Multiplot")
+
+if multiplot:
+	plt.rcParams['figure.figsize'] = (80, 80)
+else:
+	plt.rcParams['figure.figsize'] = (16, 16)
+
+
 col1, col2 = st.columns(2, gap = "large")
 
 # built-in IFS attractors
@@ -20,30 +28,36 @@ def reset_n():
 with col1:
 	option_selected = st.selectbox("Select an IFS attractor to plot",
 		box_options, on_change = reset_n)
-	if multiplot:
-		st.write("User selects number of rows and columns with max 8 plots")
-	else:
+	if multiplot == False:
 		n = st.number_input("Number of iterations: ", min_value = 0, max_value = 8,
 			step = 1, key = "n")
-		for a in attractors:
-			if a.namestring == option_selected:
-				funstrings = a.funstrings
-		for funstring in funstrings:
-			st.latex(funstring)
+	for a in attractors:
+		if a.namestring == option_selected:
+			funstrings = a.funstrings
+	for funstring in funstrings:
+		st.latex(funstring)
 
 # plot the attractor in the right column
 with col2:
 	if multiplot:
-		st.write("Draw multiplots")
+		if option_selected == "Cantor Ternary Set":
+			st.pyplot(cantor().plot(grid = (3,3)))
+		elif option_selected == "Sierpinski Gasket":
+			st.pyplot(gasket().plot(grid = (3,3)))
+		elif option_selected == "Fudgeflake":
+			st.pyplot(fudgeflake().plot(grid = (3,3), facecolor = 'b'))
+		elif option_selected == "Twindragon":
+			st.pyplot(twindragon().plot(grid = (3,3), facecolor = 'w'))
 	else:
 		if option_selected == "Cantor Ternary Set":
-			st.pyplot(cantor().plot(n = n, showaxis = True))
+			st.pyplot(cantor().plot(n = n))
 		elif option_selected == "Sierpinski Gasket":
-			st.pyplot(gasket().plot(n = n, showaxis = True))
+			st.pyplot(gasket().plot(n = n))
 		elif option_selected == "Fudgeflake":
-			st.pyplot(fudgeflake().plot(n = n, facecolor = 'b', showaxis = True))
+			st.pyplot(fudgeflake().plot(n = n, facecolor = 'b'))
 		elif option_selected == "Twindragon":
-			st.pyplot(twindragon().plot(n = n, facecolor = 'w', showaxis = True))
+			st.pyplot(twindragon().plot(n = n, facecolor = 'w'))
+
 
 st.header("Random IFS")
 
