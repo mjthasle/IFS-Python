d9e9e4bef5e2965e49dec64f11402e97f5de5ab0
emilykorfanty <ekorfant@ualberta.ca>
2025-06-10 12:20:08 -0600

    Add more pages and definition of 2D affine ifs

---
+++ pages/3_Build_Your_Own_IFS.py
@@ -0,0 +1,10 @@
+from ifs import *
+
+col1, col2 = st.columns(2, gap = "large", vertical_alignment = "bottom")
+
+# build-your-own settings in the left column
+with col1:
+
+	st.header("Build-Your-Own IFS")
+
+	st.write("Coming soon!")
