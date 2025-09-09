3f5156b79a76eaac319bb4371a515e87a02f5fa6
emilykorfanty <ekorfant@ualberta.ca>
2025-09-09 14:05:00 -0600

    Bug fix

--- pages/4_Build_Your_Own_IFS.py
+++ pages/4_Build_Your_Own_IFS.py
@@ -41,7 +41,7 @@ else:
 # Perform function deletion 
 if delete_index is not None:
     st.session_state.IFS_latex.pop(delete_index)
-    IFS_transforms.pop(delete_index)
+    # IFS_transforms.pop(delete_index)
  
     # Renumber remaining functions
     for j, tex in enumerate(st.session_state.IFS_latex, start = 1):
